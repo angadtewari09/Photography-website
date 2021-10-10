5f0ed34b4e9e30e45bef5363533f7291778cb28b
angadtewari09 <despacito0099@gmail.com>
2021-10-10 13:57:39 +0530

    gallery updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,21 +8,16 @@ import 'aos/dist/aos.css';
 
 
 const images = [
-    {id: '1' , imageName: 'img1.jpg', tag: 'free'},
-    {id: '2' , imageName: 'img2.jpg', tag: 'new'},
-    {id: '3' , imageName: 'img3.jpg', tag: 'pro'},
-    {id: '4' , imageName: 'img2.jpg', tag: 'pro'},
-    {id: '5' , imageName: 'img3.jpg', tag: 'free'},
-    {id: '6' , imageName: 'img1.jpg', tag: 'new'},
-    {id: '7' , imageName: 'img2.jpg', tag: 'pro'},
-    {id: '8' , imageName: 'img1.jpg', tag: 'free'},
-    {id: '9' , imageName: 'img3.jpg', tag: 'new'},
-    {id: '10' , imageName: 'img1.jpg', tag: 'new'},
-    {id: '11' , imageName: 'img1.jpg', tag: 'new'},
-    {id: '12' , imageName: 'img2.jpg', tag: 'free'},
-    {id: '13' , imageName: 'img3.jpg', tag: 'pro'},
-    {id: '14' , imageName: 'img2.jpg', tag: 'free'},
-    {id: '15' , imageName: 'img3.jpg', tag: 'new'}
+    {id: '1' , imageName: 'IMG_1.jpg', tag: 'free'},
+    {id: '2' , imageName: 'IMG_2.jpg', tag: 'new'},
+    {id: '3' , imageName: 'IMG_3.jpg', tag: 'pro'},
+    {id: '4' , imageName: 'IMG_4.jpg', tag: 'pro'},
+    {id: '5' , imageName: 'IMG_5.jpg', tag: 'free'},
+    {id: '6' , imageName: 'IMG_6.jpg', tag: 'new'},
+    {id: '7' , imageName: 'IMG_7.jpg', tag: 'pro'},
+    {id: '8' , imageName: 'IMG_8.jpg', tag: 'free'},
+    {id: '9' , imageName: 'IMG_9.jpg', tag: 'new'},
+   
 ];
 
 /*Filter-Buttons*/
@@ -98,9 +93,11 @@ function Main() {
                         <div className="gallery-container"> 
                             {filteredImages.map(image => {
                                 return (                               
-                                    <div key ={image.id} data-aos-duration={duration} data-aos="fade-up" className="image-card">
+                                    <div key ={image.id} data-aos-duration={duration} data-aos="fade-up" className="image-card" onClick={() => {
+                                        (document.querySelector('.NavbarItems')).style.zIndex = 0;
+                                    }}>
 
-                                    <a href={`../images/${image.imageName}`}>
+                                    <a className="fullscreen" href={`../images/${image.imageName}`}>
 
                                         <img className = "gallery-image" src={`../images/${image.imageName}`} alt= {`${image.imageName}`}/>
                                     </a>
