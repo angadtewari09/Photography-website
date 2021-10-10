a4a8205c33a4bda64ad4f419c4571e3cc6345c88
angadtewari09 <despacito0099@gmail.com>
2021-10-10 23:54:21 +0530

    few changes made

--- src/components/Main.js
+++ src/components/Main.js
@@ -73,7 +73,7 @@ function Main() {
                 </div>
                 <div data-aos-duration="1200" data-aos="fade-up" className="right">
                     <div className="profile">
-                        <p id="name2">I am John Doe</p>
+                        <p id="name2">I am John Doe.</p>
                         <p id="about-me">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose.</p>
                         <button className="butt">View Work</button>
                     </div>
